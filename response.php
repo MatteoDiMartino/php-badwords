1809d1441f7bd3576dc741666c02c3efde070655
MatteoDiMartino <matteodimartino359@gmail.com>
2024-05-01 09:09:58 +0200

    feat: add strlen function

--- response.php
+++ response.php
@@ -1,7 +1,6 @@
 <?php 
 
 $getTxt = $_GET['userTxt'];
-
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +8,21 @@ $getTxt = $_GET['userTxt'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>La tua frase</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     
-<h1> <?php echo $getTxt ?> </h1>
-
+<div class="container">
+    <div class="d-flex">
+        <div>
+           <span class="fs-1  fw-bold"> <?php echo $getTxt ?> </span>
+            <div>
+               <small class="fs-5"> <?php echo strlen($getTxt) . ' ecco la lunghezza del tuo paragrafo' ?> </small>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
